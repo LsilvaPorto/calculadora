92a3bbdfced8a38a26e38ade7f6260087c44fa55
Lucas Porto <lucasportojpa@gmail.com>
2019-01-10 23:36:19 -0200

    calculadora totalmente funcional

--- src/Logic/Logica.java
+++ src/Logic/Logica.java
@@ -2,14 +2,76 @@ package Logic;
 
 public class Logica {
 
-	public static int num1;
-	public static int num2;
+	private float num1;
+	private float num2;
+	public static int idOperacao = 0;
+	public static float resultado;
 	
-	public void salvaNum1(String texto) {
-		String x = texto.replace("+", "");
-		int num = Integer.parseInt(x);
-		num1 = num;
-		System.out.println(num1);
+	public void clearNuns() {
+		num1 = 0;
+		num2 = 0;
+		idOperacao = 0;
+	}
+	
+	public float getNum1() {
+		return num1;
 	}
 
+	public float soma(float x, float y) {
+		num1 = x;
+		num2 = y;
+		return (num1 + num2);
+
+	}
+
+	public float sub(float x, float y) {
+		num1 = x;
+		num2 = y;
+		return (num1 - num2);
+	}
+
+	public float multi(float x, float y) {
+		num1 = x;
+		num2 = y;
+		return (num1 * num2);
+	}
+
+	public float div(float x, float y) {
+		num1 = x;
+		num2 = y;
+		return (num1 / num2);
+	}
+
+	public void salvaNum1(String texto) {
+		if (idOperacao == 0) {
+			float num = Float.parseFloat(texto);
+			num1 = num;
+		}
+	}
+
+		public float percent(float x) {
+			return x/100;
+		}
+
+	public void salvaNum2(String texto) {
+		float num = Float.parseFloat(texto);
+		num2 = num;
+	}
+
+	public void checkOperacao() {
+		
+		if (idOperacao == 1) {
+			soma(num1, num2);
+			resultado = soma(num1, num2);
+		} else if (idOperacao == 2) {
+			sub(num1, num2);
+			resultado = sub(num1, num2);
+		} else if (idOperacao == 3) {
+			multi(num1, num2);
+			resultado = multi(num1, num2);
+		} else if (idOperacao == 4) {
+			div(num1, num2);
+			resultado = div(num1, num2);
+		}
+	}
 }
--- src/windows/Main.java
+++ src/windows/Main.java
@@ -13,6 +13,7 @@ import javax.swing.border.EmptyBorder;
 import Logic.Logica;
 import javax.swing.DropMode;
 import java.awt.Font;
+import java.awt.TextField;
 
 public class Main extends JFrame {
 
@@ -36,6 +37,10 @@ public class Main extends JFrame {
 	private JButton button_12;
 	private String texto;
 	public Logica l = new Logica();
+	private JButton button_13;
+	private JButton button_14;
+	private JButton button_15;
+	private boolean fim = false;
 
 	/**
 	 * Launch the application.
@@ -58,7 +63,7 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 560);
+		setBounds(100, 100, 450, 650);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -73,6 +78,7 @@ public class Main extends JFrame {
 		button = new JButton("1");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "1");
 			}
@@ -83,6 +89,7 @@ public class Main extends JFrame {
 		button1 = new JButton("2");
 		button1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "2");
 			}
@@ -93,6 +100,7 @@ public class Main extends JFrame {
 		button2 = new JButton("3");
 		button2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "3");
 			}
@@ -104,12 +112,13 @@ public class Main extends JFrame {
 		button3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				texto = textField.getText();
-				if (!texto.contains("+")) {
-					String x = texto + "+";
-					textField.setText(x);
+				if (l.getNum1() == 0) {
+					textField.setText("");
 					l.salvaNum1(texto);
+					l.idOperacao = 1;
 
 				}
+
 			}
 		});
 		button3.setBounds(319, 419, 91, 67);
@@ -118,6 +127,7 @@ public class Main extends JFrame {
 		button_1 = new JButton("4");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "4");
 			}
@@ -128,6 +138,7 @@ public class Main extends JFrame {
 		button_2 = new JButton("5");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "5");
 			}
@@ -138,6 +149,7 @@ public class Main extends JFrame {
 		button_3 = new JButton("6");
 		button_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "6");
 			}
@@ -146,12 +158,23 @@ public class Main extends JFrame {
 		contentPane.add(button_3);
 
 		button_4 = new JButton("-");
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				texto = textField.getText();
+				if (l.getNum1() == 0) {
+					textField.setText("");
+					l.salvaNum1(texto);
+					l.idOperacao = 2;
+				}
+			}
+		});
 		button_4.setBounds(319, 329, 91, 67);
 		contentPane.add(button_4);
 
 		button_5 = new JButton("7");
 		button_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "7");
 			}
@@ -162,6 +185,7 @@ public class Main extends JFrame {
 		button_6 = new JButton("8");
 		button_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "8");
 			}
@@ -172,6 +196,7 @@ public class Main extends JFrame {
 		button_7 = new JButton("9");
 		button_7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				checkFim();
 				texto = textField.getText();
 				textField.setText(texto + "9");
 			}
@@ -180,6 +205,17 @@ public class Main extends JFrame {
 		contentPane.add(button_7);
 
 		button_8 = new JButton("*");
+		button_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				texto = textField.getText();
+				if (l.getNum1() == 0) {
+					textField.setText("");
+					l.salvaNum1(texto);
+					l.idOperacao = 3;
+				}
+
+			}
+		});
 		button_8.setBounds(319, 235, 91, 67);
 		contentPane.add(button_8);
 
@@ -210,11 +246,78 @@ public class Main extends JFrame {
 		contentPane.add(button_10);
 
 		button_11 = new JButton("%");
+		button_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				texto = textField.getText();
+					float x = Float.parseFloat(texto);
+					l.percent(x);
+					textField.setText(""+l.percent(x));
+					
+			}
+		});
 		button_11.setBounds(216, 142, 91, 67);
 		contentPane.add(button_11);
 
 		button_12 = new JButton("/");
+		button_12.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				texto = textField.getText();
+				if (l.getNum1() == 0) {
+					textField.setText("");
+					l.salvaNum1(texto);
+					l.idOperacao = 4;
+				}
+			}
+		});
 		button_12.setBounds(319, 142, 91, 67);
 		contentPane.add(button_12);
+
+		button_13 = new JButton("0");
+		button_13.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				texto = textField.getText();
+				textField.setText(texto + "0");
+			}
+		});
+		button_13.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_13.setBounds(12, 509, 194, 67);
+		contentPane.add(button_13);
+
+		button_14 = new JButton(",");
+		button_14.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				texto = textField.getText();
+				if (!texto.contains(".")) {
+					textField.setText(texto + ".");
+				}
+			}
+		});
+		button_14.setBounds(216, 509, 91, 67);
+		contentPane.add(button_14);
+
+		button_15 = new JButton("=");
+		button_15.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String texto = textField.getText();
+				l.salvaNum2(texto);
+				textField.setText("");
+				l.checkOperacao();
+				String x = "" + l.resultado;
+				textField.setText(x);
+				l.clearNuns();
+				fim = true;
+			}
+		});
+		button_15.setBounds(319, 509, 91, 67);
+		contentPane.add(button_15);
+	}
+	void checkFim() {
+		if (fim) {
+			textField.setText("");
+			fim = false;
+		}
 	}
 }
